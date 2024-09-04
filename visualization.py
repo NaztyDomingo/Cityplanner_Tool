05cfd74f654da95de3da3879d00363958dcf120c
sinivuorensyrja <sini.vuorensyrja@brightstraining.com>
2024-09-04 12:21:19 +0300

    separated dash app file and data handler file for visualization

--- visualization.py
+++ visualization.py
@@ -5,45 +5,14 @@ import dash
 from dash import Dash, dcc, html, Input, Output, State, callback_context
 import plotly.graph_objects as go
 import pandas as pd
-
-# Load and prepare data
-def load_data():
-    
-    # Read csv files to get data
-    # Finland data
-    filepath = fh.get_path_of_folder('transformed_finland_data')
-    filepath_of_fin_cities = os.path.join(filepath, 'finland_cities_emissions.csv')
-    filepath_of_fin_regions = os.path.join(filepath, 'finland_regions_emissions.csv')
-    fin_cities_df = pd.read_csv(filepath_of_fin_cities)
-    fin_regions_df = pd.read_csv(filepath_of_fin_regions)
-
-    # Sweden data
-    # TBA
-
-    # Combine SWE+FIN city data
-    # Combine SWE+FIN region data ?
-
-    # Tree data
-    filepath = fh.get_path_of_folder('transformed_tree_data')
-    filepath_of_trees = os.path.join(filepath, 'final_tree_info.csv')
-    tree_df = pd.read_csv(filepath_of_trees)
-
-    # Dash example:
-    df = pd.DataFrame({
-        'Category': ['A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'D'],
-        'Variable': ['X', 'Y', 'Z', 'X', 'Y', 'Z', 'X', 'Y', 'Z', 'X', 'Y', 'Z'],
-        'Values': [4, 3, 6, 1, 5, 7, 2, 8, 5, 5, 7, 6]
-    })
-
-    list_of_dfs = [fin_cities_df, fin_regions_df, tree_df, df]
-    return list_of_dfs
+import visualization_data as vd
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
 app.title = "Dash Data Visualization"
 
 # Load and unpack dfs
-dfs = load_data()
+dfs = vd.load_data()
 fin_cities_df, fin_regions_df, tree_df, df = dfs
 
 # Define the layout of the app
