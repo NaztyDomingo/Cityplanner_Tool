744f172f358b0899769d7670edff8a0dba1338a6
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-09-06 15:17:38 +0200

    now able to load all transformed data files to database

--- filehandler_helper.py
+++ filehandler_helper.py
@@ -18,6 +18,11 @@ def get_path_of_folder(folder: str) -> str:
 def get_list_with_names_from_folder(filepath: str) -> np.array:
     return os.listdir(filepath)
 
+def get_list_with_files_from_folder(filepath: str) -> dict:
+    files_and_folder = os.listdir(filepath)
+    files = [(os.path.splitext(file)[0], os.path.join(filepath, file)) for file in files_and_folder if os.path.isfile(os.path.join(filepath, file))]
+    return files
+
 def get_current_filepath() -> str:
     return os.path.dirname(os.path.realpath(__file__))
 
@@ -29,3 +34,13 @@ def search_folder_get_list_with_foldernames(search_string: str, filepath_to_fold
     folders = get_list_with_names_from_folder(filepath_to_folder_to_search)
     matching_folders = [folder for folder in folders if regex.search(folder)]
     return matching_folders
+
+def get_tupled_list_with_filename_and_filepath_from_list_with_folder_names(list_with_folder_names: list) -> list:
+    list_with_files = []
+
+    for foldername in list_with_folder_names:
+        list_with_files.append(get_list_with_files_from_folder(get_path_of_folder(foldername)))
+
+    list_without_empty_lists = [file for file in list_with_files if file != []]
+    flatten_list = [file for sublist in list_without_empty_lists for file in sublist]
+    return flatten_list
