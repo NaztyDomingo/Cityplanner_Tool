afe4deb95681e6c097316e667108933fa5a7d5aa
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-09-06 13:15:59 +0200

    fixed typo with finish naming

--- load_data.py
+++ load_data.py
@@ -6,7 +6,7 @@ import psycopg2
 
 def main() -> None:
     db_username, db_password = get_db_username_and_password('DEV', 'cityplanner_db_username', 'cityplanner_db_password')
-    _load_data(db_username, db_password, get_db_name)
+    to_database(db_username, db_password, get_db_name)
 
 def get_db_name() -> str:
     return 'cityplanner_db'
@@ -54,20 +54,24 @@ def insert_table(df: pd.DataFrame, connection_instance: psycopg2.connect) -> Non
     cur.close()
     connection_instance.close()
 
-def to_database(df: pd.DataFrame, db_username: str, db_password: str, db_name: str, port_number : int = 5432, hostname : str = 'localhost') -> None:
+"""def to_database(df: pd.DataFrame, db_username: str, db_password: str, db_name: str, port_number : int = 5432, hostname : str = 'localhost') -> None:
     engine = _create_engine(db_username, db_password, db_name, port_number, hostname)
 
     df.to_sql(name ='finland_regions_emissions', con = engine, if_exists ='replace', index=False)
-
-def _load_data(db_username: str, db_password: str, db_name: str, port_number : int = 5432, hostname : str = 'localhost') -> None:
+"""
+def to_database(db_username: str, db_password: str, db_name: str, port_number : int = 5432, hostname : str = 'localhost') -> None:
     folder_name = 'transformed_finland_data'
-    filename = 'finland_regions_emissions.csv'
-    filepath = fh.get_path_of_file(folder_name, filename)
-    df = pd.read_csv(filepath)
     
-    engine = _create_engine(db_username, db_password, db_name, port_number, hostname)
+    filepath = fh.get_path_of_folder(folder_name)
+    list_with_folder_names = fh.get_list_with_names_from_folder(filepath)
 
-    df.to_sql(name ='finland_regions_emissions', con = engine, if_exists ='replace', index=False)
+    for filename in list_with_folder_names:
+        filepath = fh.get_path_of_file(folder_name, filename) # CHANGE
+        df = pd.read_csv(filepath)
+        
+        engine = _create_engine(db_username, db_password, db_name, port_number, hostname)
+
+        df.to_sql(name = filename, con = engine, if_exists ='replace', index=False)
 
 
 if __name__ == "__main__":
