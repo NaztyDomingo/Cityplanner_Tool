f226c7167a6fa9717e80940b597e70656553e0f8
Tavyah <94623622+Tavyah@users.noreply.github.com>
2024-09-09 08:58:22 +0200

    updated load data with capabilities to pull from database

--- load_data.py
+++ load_data.py
@@ -7,6 +7,8 @@ import psycopg2
 def main() -> None:
     db_username, db_password = get_db_username_and_password('DEV', 'cityplanner_db_username', 'cityplanner_db_password')
     to_database(db_username, db_password, get_db_name())
+    l = pull_all_tables_return_list_with_dfs(db_username, db_password, get_db_name())
+    print(l)
 
 def get_db_name() -> str:
     return 'cityplanner_db'
@@ -44,6 +46,22 @@ def to_database(db_username: str, db_password: str, db_name: str, port_number : 
         engine = _create_engine(db_username, db_password, db_name, port_number, hostname)
 
         df.to_sql(name = filename, con = engine, if_exists ='replace', index=False)
+
+def select_table(table_name: str, engine: create_engine) -> pd.DataFrame:
+    df = pd.read_sql_query(f'SELECT * FROM {table_name}', con=engine)
+    return df
+
+def pull_all_tables_return_list_with_dfs(db_username: str, db_password: str, db_name: str, port_number : int = 5432, hostname : str = 'localhost') -> None:
+    filepath = fh.get_path_of_folder('')
+    list_with_folder_names = fh.search_folder_get_list_with_foldernames('transformed_', filepath)
+    list_with_files_to_load = fh.get_tupled_list_with_filename_and_filepath_from_list_with_folder_names(list_with_folder_names)
+
+    engine = _create_engine(db_username, db_password, db_name, port_number, hostname)
+    list_with_dfs = []
+    for filename, filepath in list_with_files_to_load:
+        list_with_dfs.append(select_table(filename, engine))
+
+    return list_with_dfs
 
 if __name__ == "__main__":
     main()
